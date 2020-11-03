8473e1ea33d9cd33a24c24de92ca8e0c1c58015d
jaizon <jaizoncarlos@protonmail.com>
2020-11-03 13:17:22 -0300

    fixed sources and names
    Fixed some files being loaded even thoug they were not audio files, now they are loaded based on original files and extension (they were previously loaded based on original files alone)
    
    Names now come from file names instead of file's titles, that makes them more consistent since they don't rely on original files metadata anymore(in case of audio files the last 4 characters are being excluded so that the file extension don't appear)

--- custom/script.js
+++ custom/script.js
@@ -6,7 +6,6 @@ const sourceInfo = document.getElementById("fyp-source-info");
 const musicImage = document.getElementById("fyp-music-image");
 const progressBar = document.getElementById("fyp-progress-bar");
 
-
 //--Button Components
 const btPlay = document.getElementById("fyp-button-play");
 const btPrev = document.getElementById("fyp-button-prev");
@@ -17,130 +16,134 @@ const btI = document.getElementById("fyp-button-mute-i");
 const btMode = document.getElementById("fyp-sources");
 const btM = document.getElementById("fyp-button-mode");
 
-
-
-
 // Starters
 let current = 0;
 player.volume = volume.value;
 loadData();
 init();
 
-
-
-
 //Add event listener on player and play button for controlling play button and other things
-player.addEventListener('playing', function() {
+player.addEventListener("playing", function () {
     btPlay.innerHTML = '<i class="fa fa-pause"></i>';
 });
 
-player.addEventListener('pause', function() {
+player.addEventListener("pause", function () {
     btPlay.innerHTML = '<i class="fa fa-play"></i>';
 });
 
-player.addEventListener('ended', function() {
+player.addEventListener("ended", function () {
     btPlay.innerHTML = '<i class="fa fa-spinner circle"></i>';
     next();
 });
 
-btPlay.addEventListener('click', function() {
+btPlay.addEventListener("click", function () {
     this.innerHTML == '<i class="fa fa-play"></i>'
-        ? player.play() : player.pause()
+        ? player.play()
+        : player.pause();
 });
-
-
 
 //Buttons event listeners
 //--Volume
-volume.addEventListener('change', function(){
+volume.addEventListener("change", function () {
     player.volume = volume.value;
 });
 
-btMute.addEventListener('click', function() {
-    btI.classList.contains('fa-volume-off')
-        ? btI.className = 'fa fa-volume-slash' : btI.className = 'fa fa-volume-off'
+btMute.addEventListener("click", function () {
+    btI.classList.contains("fa-volume-off")
+        ? (btI.className = "fa fa-volume-slash")
+        : (btI.className = "fa fa-volume-off");
 
-    !btI.classList.contains('fa-volume-off')
-        ? player.muted = true : player.muted = false
-}); 
+    !btI.classList.contains("fa-volume-off")
+        ? (player.muted = true)
+        : (player.muted = false);
+});
 
 //--Next and prev
-btNext.addEventListener('click', function() {
+btNext.addEventListener("click", function () {
     next();
 });
 
-btPrev.addEventListener('click', function() {
+btPrev.addEventListener("click", function () {
     prev();
-})
-
-//--Modes
-btRand.addEventListener('click', function() {
-    this.classList.toggle('toggled');
 });
 
-btMode.addEventListener('change', function() {
-    current = 0
+//--Modes
+btRand.addEventListener("click", function () {
+    this.classList.toggle("toggled");
+});
+
+btMode.addEventListener("change", function () {
+    current = 0;
     loadData();
     init();
     btMode.value === "radios"
-        ? btM.innerHTML = "<i class=\"fa fa-podcast\"></i>"
-        : btM.innerHTML = "<i class=\"fa fa-music\"></i>"
+        ? (btM.innerHTML = '<i class="fa fa-podcast"></i>')
+        : (btM.innerHTML = '<i class="fa fa-music"></i>');
 });
-
-
 
 //Functions
 //--Functionalities
 function loadData() {
-    data = []
+    data = [];
     btMode.value === "radios"
-        ? data = radios
+        ? (data = radios)
         : fetch(`https://archive.org/metadata/${btMode.value}`)
-            .then(response => response.json())
-                .then(json => {
-                    for(let item in json.files){
-                        json.files[item].source == "original"
-                            ? data.push(json.files[item])
-                            : void 0
-                    } 
-                    return data
-                })    
+              .then((response) => response.json())
+              .then((json) => {
+                  for (let item in json.files) {
+                      json.files[item].source === "original" &&
+                          (json.files[item].name.slice(-3) === "mp3" ||
+                              json.files[item].name.slice(-3) === "ogg" ||
+                              json.files[item].name.slice(-3) === "m4a") &&
+                          data.push(json.files[item]);
+                  }
+                  return data;
+              });
 }
 
 function init() {
-    btRand.classList.contains('toggled')
-        ? current = Math.floor(Math.random() * data.length)
-        : void 0
+    btRand.classList.contains("toggled")
+        ? (current = Math.floor(Math.random() * data.length))
+        : void 0;
 
     data[current]
-        ?(
-            player.pause(),
-            player.setAttribute('src', `${
-                btMode.value == "radios" 
-                    ? data[current].file 
-                    : `https://archive.org/download/${btMode.value}/${data[current].name}`
-            }`),
-            player.load(),
-            player.play(),
-            timer(),
-            sourceInfo.innerHTML = `<span id="fyp-source-title">${data[current].title}</span>
-            <span id="fyp-source-artist">${data[current].artist == undefined ? "Radio" : data[current].artist }</span>`
-        ):
-           setTimeout(init, 500)
+        ? (player.pause(),
+          player.setAttribute(
+              "src",
+              `${
+                  btMode.value === "radios"
+                      ? data[current].file
+                      : `https://archive.org/download/${btMode.value}/${data[current].name}`
+              }`
+          ),
+          player.load(),
+          player.play(),
+          timer(),
+          (sourceInfo.innerHTML = `<span id="fyp-source-title">${
+              btMode.value === "radios"
+                  ? data[current].name
+                  : data[current].name.slice(0, -4)
+          }</span>
+            <span id="fyp-source-artist">${
+                data[current].artist == undefined
+                    ? "Radio"
+                    : data[current].artist
+            }</span>`))
+        : setTimeout(init, 500);
 }
 
 function timer() {
-    player.addEventListener('timeupdate', function(){
-        progressBar.style.width = ((player.currentTime / player.duration) * 100) + "%"
-    })
+    player.addEventListener("timeupdate", function () {
+        progressBar.style.width =
+            (player.currentTime / player.duration) * 100 + "%";
+    });
 }
 
 //--Controls
 function next() {
-    current >= data.length - 1
-        ?( current = 0, init()):(current++,  init())
+    current >= data.length - 1 ? ((current = 0), init()) : (current++, init());
 }
 
 function prev() {
-    current <= 0 ? (current = data.length - 1, init()):(current--, init())
+    current <= 0 ? ((current = data.length - 1), init()) : (current--, init());
 }
